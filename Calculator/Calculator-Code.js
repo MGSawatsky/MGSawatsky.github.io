84b92cb77ec4b341c4c364a2e9c8fb3db17c310f
Mitchell <46569264+MGSawatsky@users.noreply.github.com>
2019-02-09 13:05:30 -0700

    Calculator 2.0
    basically done

--- Calculator/Calculator-Code.js
+++ Calculator/Calculator-Code.js
@@ -1,17 +1,73 @@
 
-const calculator = document.querySelector('Calculator-Class');
-//var display = document.getElementById('display')
-const keys = calculator.querySelector('Calculator-Body');
+const calculator = {
+  currentDisplay: '0',
+  currentString: "0",
+  firstPress: true,
+};
 
-keys.addEventListener('click', e => {
- if (e.target.matches('button')) {
-   const key = e.target
-   const action = key.dataset.action
+const buttons = document.querySelector('.Calculator-Body');
 
-   if (
-     action==='add' || action==='subtract' || action==='multiply' || action==='divide'
-   ) {
-     console.log('key')
-   }
- }
+function updateDisplay(){
+  const display = document.querySelector('.Calculator-Display');
+  display.value = calculator.currentDisplay;
+}
+
+updateDisplay();
+
+function numberFunction(number){
+  //Makes it function as you would normally expect
+  if(calculator.firstPress){
+    calculator.currentString = number;
+    calculator.firstPress=false;
+  }
+  else{
+    calculator.currentString += number;
+  }
+  calculator.currentDisplay = calculator.currentString;
+  updateDisplay();
+}
+
+function clearFunction(){
+  calculator.currentString ="0";
+  calculator.currentDisplay = '0';
+  calculator.firstPress = true;
+  updateDisplay();
+}
+
+function equalsFunction(){
+  try{
+    calculator.currentDisplay = eval(calculator.currentString);
+    console.log(calculator.currentDisplay);
+    if(calculator.currentDisplay=='NaN' || calculator.currentDisplay=="undefined" ||  calculator.currentDisplay=="Infinity" || calculator.currentDisplay=="function Error() { [native code] }"){
+      throw "err";
+    }
+  }
+  catch(err){
+    calculator.currentDisplay = "Error";
+    calculator.currentString = '0';
+    calculator.firstPress = true;
+  }
+  updateDisplay();
+  calculator.currentString = calculator.currentDisplay;
+}
+
+buttons.addEventListener('click', (event) => {
+  const {target} = event;
+
+  if(!target.matches('button')){
+    console.log("not clicking a button")
+    return;
+  }
+  else if(target.classList.contains('clear')){
+    //do clear function
+    clearFunction();
+  }
+  else if(target.classList.contains('equals')){
+    //do equals function
+    equalsFunction();
+  }
+  else{
+    numberFunction(target.value);
+    //must be a number
+  }
 })
