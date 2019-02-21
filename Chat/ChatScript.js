73d65e677def16cfce50685825a51b47b76f50fb
Mitchell <46569264+MGSawatsky@users.noreply.github.com>
2019-02-20 17:51:50 -0700

    update

--- Chat/ChatScript.js
+++ Chat/ChatScript.js
@@ -1,2 +1,7 @@
 
 var userCount = 0;
+
+function updateUser(){
+    var username = document.getElementById("username").innerHTML="USER"+userCount;
+    userCount = userCount+1;
+}
--- Chat/index.js
+++ Chat/index.js
@@ -11,7 +11,6 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-
 // app.get('/', function(req, res){
 //   res.sendFile(__dirname + '/index.html');
 // });
